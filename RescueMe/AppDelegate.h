6479bb27be78d4993a56800b68cb19bd92cf9191
Ahmad Samra <rehabaam@gmail.com>
2014-06-01 11:21:37 +0400

    First upload
    RescueMe iOS application supporting Arabic localisation.

---
+++ RescueMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RescueMe
+//
+//  Created by Ahmad Samra on 5/31/14.
+//  Copyright (c) 2014 Ahmad Samra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
